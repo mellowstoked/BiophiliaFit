1687af9a8c4fae619fb9b5323c175b049273d258
Cody Mckeon <arachnid.webdev@gmail.com>
2016-02-10 16:48:49 -0800

    Deleted instagram API

--- config/initializers/instagram.rb
+++
@@ -1,5 +0,0 @@
-require "instagram"
-  Instagram.configure do |config|
-    config.client_id = "24b4cccc45834d24bc654aef46fbdddf"
-    config.access_token = "174431290.24b4ccc.39692632ecf244a39271817077bdb35d"
-  end
